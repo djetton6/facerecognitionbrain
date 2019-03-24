66a51aa4ffe2465c17a2be9f779867008f8f2016
Desmend Jetton <djetton@iit.edu>
2019-03-24 00:59:26 -0500

    included brain logo and styling for logo component.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
+ import 'tachyons';
 
-import 'tachyons';
 class App extends Component {
   render() {
     return (
--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Tilt from 'react-tilt';
+import './logo.css';
+import brain from './brain.png'
+
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 25 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner"><img alt='brainfood' src={brain}></img>
+                </div>
+            </Tilt>
         </div>
     );
 }
