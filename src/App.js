68809819d8b6bb85f2534b709c134e3e61b34c86
Desmend Jetton <djetton@iit.edu>
2019-03-24 00:37:42 -0500

    imported new components for logo and navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
+
+import 'tachyons';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo />
-        <ImageLinkForm />
-        <FaceRecognition />
-         */}
+        <Logo />
+        {/* // <ImageLinkForm />
+        // <FaceRecognition />
+        //  */} */}
       </div>
     );
   }
