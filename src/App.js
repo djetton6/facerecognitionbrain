47f0e051af0910a97d45fb6a5c5aab7236d49038
Desmend Jetton <djetton@iit.edu>
2019-03-25 02:05:38 -0500

    inserted and styled particles.js to json

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imagelinkform/imagelinkform'
 import Rank from './components/rank/rank'
+import Particles from 'react-particles-js';
 
 import 'tachyons';
+
+const particplesOptions = {
+
+  particles: {
+    number: {
+      value: 300,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particplesOptions}
+
+        />
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm />
-        
+
         {
           /* <FaceRecognition />*/}
       </div>
--- src/components/rank/rank.js
+++ src/components/rank/rank.js
@@ -3,14 +3,12 @@ import React from 'react';
 const Rank = () => {
     return (
         <div>
-            <p className='f3'>
-                {'This Brain will find the faces in pictures. Give it a try!'}
-            </p>
-            <div className='center'>
-                <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='tex' />
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-purple'>Demo</button>
-                </div>
+            <div className='white f3'>
+                {'Desmend, your current rank is.....'}
+            </div>
+
+            <div className="white f1">
+                {'#5'}
             </div>
         </div>
     );
