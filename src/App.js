6bc1ccca9751773b5c9517b43d1b73b9ff1def3a
Desmend Jetton <djetton@iit.edu>
2019-03-25 23:34:43 -0500

    added Clarifai API, styled image box, and received API key.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imagelinkform/imagelinkform'
 import Rank from './components/rank/rank'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/facerecognition/facerecognition'
 
 import 'tachyons';
+
+const app = new Clarifai.App({
+  apiKey: 'a8467f43f8e448c3ae456a7175b9f94b'
+});
 
 const particplesOptions = {
 
@@ -22,6 +28,30 @@ const particplesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function (response) {
+        console.log(response);
+      },
+      function (err) {
+        // there was an error
+      }
+    );
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -32,10 +62,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSumbit={this.onButtonSubmit} />
 
-        {
-          /* <FaceRecognition />*/}
+        <FaceRecognition />
       </div>
     );
   }
