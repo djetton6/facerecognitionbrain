c6f974beae1db9b0bd5c7b9e0f39a60b8b7e2106
Desmend Jetton <djetton@iit.edu>
2019-03-28 23:32:05 -0500

    created box to shift around photo's identified faces.

--- src/components/facerecognition/facerecognition.js
+++ src/components/facerecognition/facerecognition.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'></div>
-            <img alt='new faces' src={imageUrl} width='500px' height='auto' />
+            <img alt='inputimage' src={imageUrl} width='500px' height='auto' />
+            <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: bottomRow, left: leftCol }}></div>
         </div>
     );
 }
